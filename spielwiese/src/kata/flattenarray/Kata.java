9058ecbc76a00637fdf69d02bc0b964b23ffd934
Karsten Scholz <ks@frankcom.info>
2020-09-26 14:44:15 +0200

    Organize imports.

--- spielwiese/src/kata/flattenarray/Kata.java
+++ spielwiese/src/kata/flattenarray/Kata.java
@@ -1,13 +1,12 @@
 package kata.flattenarray;
 
-import java.util.Arrays;
 import java.util.function.IntUnaryOperator;
 
 public class Kata {
 
 	  public static IntUnaryOperator add(int n) {
 		
-		  return i -> i +n;
+		  return i -> i + n;
 	}
 
 }
