01ca8d3977ef324c08bce77ece89e05b996c75c7
Karsten Scholz <ks@frankcom.info>
2020-09-27 13:32:07 +0200

    Organize Import

--- spielwiese/src/kata/flattenarray/SolutionTest.java
+++ spielwiese/src/kata/flattenarray/SolutionTest.java
@@ -1,11 +1,8 @@
 package kata.flattenarray;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
-import org.junit.Assert;
 
 public class SolutionTest {
 
